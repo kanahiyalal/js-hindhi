4e95fbb601afd7551bea80665bb6629b59d55e3c
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-18 20:20:45 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat  all js code as a newer version
+
+//alert(3 + 3) //we are using node js , not browser
+
+let name = "kanahiya"
+let age = 20
+let isLoggedIn = true
+
+
+
+//Data Types
+//number => 2 to power 53
+//bigint 
+//string => ""
+//Boolean true or false
+//null = standloan value    //empty
+//undefined => 
+// symbol => unique
+
+console.log(typeof age)
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
