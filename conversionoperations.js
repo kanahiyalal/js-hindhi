67c40ca2a4c21173ed7144bf0bf554f7d71ea23d
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-19 09:35:31 +0000

    conversions of datatypes

---
+++ conversionoperations.js
@@ -0,0 +1,28 @@
+let score = "33";
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN 
+// true => 1   //false => 0
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanIsLoggedIn);
+
+// 1 => true              // 0 => false 
+// "" => false           // "kanahiya"  true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
