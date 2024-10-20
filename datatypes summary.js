b922f2e1e655f2f26f20731ac8de47f186ef7b60
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-20 09:57:12 +0000

    summary

---
+++ datatypes summary.js
@@ -0,0 +1,15 @@
+// primitive 
+// 7-types :string,int,float,boolean,number,null,undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+const id = symbol('123')
+const anotherId = symbol('123')
+console.log(id === anotherId);
+
+
+//reference type (non-primitive)
+//array,objects,function
