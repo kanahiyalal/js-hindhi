62e45fb14a144955e9f23fdc96382d9cdf9dcced
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-21 17:02:27 +0000

    datatypes

--- datatypes summary.js
+++ datatypes summary.js
@@ -1,5 +1,6 @@
 // primitive 
-// 7-types :string,int,float,boolean,number,null,undefined,symbol,BigInt
+// 7-types :string,int,float,boolean,number,null,undefined,symbol,BigInt     //call by value
+//symbol kisi bhi chez ko unique banana ka leya symbol use keya jata ha
 
 const score = 100
 const scoreValue = 100.3
@@ -10,6 +11,18 @@ const id = symbol('123')
 const anotherId = symbol('123')
 console.log(id === anotherId);
 
+const BigNumber = 1234463827289779789n
+
 
 //reference type (non-primitive)
-//array,objects,function
+//array,objects,functions
+
+const heeros = ["saalmankhn" , "sharukhkhan", "saktiman"]
+let myObj = {
+    name: "kanahiya",
+    age : 20,
+}
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof BigNumber);
