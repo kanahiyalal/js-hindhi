a2c33118cebc89b339daff9d42d6e8ad8ac9e099
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-25 18:05:59 +0000

    javascripy

---
+++ string.js
@@ -0,0 +1,6 @@
+const name = "kanahiya"
+const rapoCount = 50
+// console.log(name + rapoCount + "value");
+
+console.log(`hello my  name is ${name} and my rapo count is ${rapoCount}`);
+const gameName = new String ('kanahiya soni');
