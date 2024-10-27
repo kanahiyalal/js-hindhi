a723648035ef799250721a7b7aa2a703f7350572
kanahiyalal <kanahiyalal690@gmail.com>
2024-10-27 16:00:57 +0000

    string

--- string.js
+++ string.js
@@ -1,6 +1,42 @@
+
+//Rapocount
 const name = "kanahiya"
 const rapoCount = 50
 // console.log(name + rapoCount + "value");
+// console.log(`hello my  name is ${name} and my rapo count is ${rapoCount}`);
 
-console.log(`hello my  name is ${name} and my rapo count is ${rapoCount}`);
-const gameName = new String ('kanahiya soni');
+
+//proto  
+const gameName = new String ("kanahiya soni");
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+
+//length , touppercase, charAt, indexOf
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt());
+console.log(gameName.indexOf('n'));
+
+
+//substring
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+//slice  
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+//trim
+const newStringOne = "    kanahiya  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+
+
+
+
+
